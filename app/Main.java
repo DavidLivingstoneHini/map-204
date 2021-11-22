235f65dca44553234e87d4b9761c5eccceb753b0
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-11-22 17:35:05 +0000

    204

--- app/Main.java
+++ app/Main.java
@@ -15,13 +15,16 @@ public class Main {
     public static void main(String[] args) {
         Digraph graph = new Digraph();
         // Graph graph = new Graph();
-        Node a = new Node("Main Gate");
-        Node b = new Node("Balme Library");
-        Node c = new Node("Commonwealth Hall");
-        Node d = new Node("Great Hall");
-        Node e = new Node("Night Market");
-        Node f = new Node("CC");
-        Node g = new Node("Diaspora");
+        Node a = new Node("Great Hall");
+        Node b = new Node("Commonwealth Hall");
+        Node c = new Node("Balme Library");
+        Node d = new Node("JQB");
+        Node e = new Node("Main Gate");
+        Node f = new Node("Banking Square");
+        Node g = new Node("CC");
+        Node h = new Node("Sarbah Field");
+        Node i = new Node("Night Market");
+        Node j = new Node("Diaspora");
 
         graph.addNode(a);
         graph.addNode(b);
@@ -30,23 +33,32 @@ public class Main {
         graph.addNode(e);
         graph.addNode(f);
         graph.addNode(g);
+        graph.addNode(h);
+        graph.addNode(i);
+        graph.addNode(j);
 
-        graph.addEdge(new Edge(a, b, 3));
-        graph.addEdge(new Edge(a, c, 8));
-        graph.addEdge(new Edge(a, g, 11));
+        graph.addEdge(new Edge(a, b, 1));
+        graph.addEdge(new Edge(b, c, 1));
+        graph.addEdge(new Edge(c, d, 2));
 
-        graph.addEdge(new Edge(b, d, 22));
-        graph.addEdge(new Edge(b, c, 3));
+        graph.addEdge(new Edge(d, e, 1));
+        graph.addEdge(new Edge(e, f, 3));
 
-        graph.addEdge(new Edge(c, d, 1));
-        graph.addEdge(new Edge(c, e, 7));
+        graph.addEdge(new Edge(b, h, 2));
+        graph.addEdge(new Edge(b, g, 1));
+        graph.addEdge(new Edge(c, g, 2));
 
-        graph.addEdge(new Edge(d, f, 7));
-        graph.addEdge(new Edge(d, e, 9));
+        graph.addEdge(new Edge(g, f, 3));
+        graph.addEdge(new Edge(g, h, 2));
+        graph.addEdge(new Edge(h, g, 2));
+        graph.addEdge(new Edge(g, h, 2));
 
-        graph.addEdge(new Edge(e, f, 5));
-        graph.addEdge(new Edge(e, g, 1));
-        graph.addEdge(new Edge(g, e, 1));
+        graph.addEdge(new Edge(h, i, 3));
+        graph.addEdge(new Edge(i, h, 3));
+        graph.addEdge(new Edge(f, i, 1));
+
+        graph.addEdge(new Edge(i, j, 2));
+        graph.addEdge(new Edge(j, i, 2));
 
         System.out.println("Please choose your current location:");
         graph.listPlaces(null);
